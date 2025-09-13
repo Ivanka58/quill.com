e9197bace911dbc2d5f18a9e650557dee10f42bc
Ivanka58 <kalamaevi0@gmail.com>
2025-09-13 16:20:34 +0300

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,1 +1,17 @@
+// js/main.js
+import { goToCodeScreen, confirmCode } from './auth.js';
 
+const phoneScreenButton = document.getElementById('phoneScreenButton');
+const confirmCodeButton = document.getElementById('confirmCodeButton');
+const phoneScreen = document.getElementById('phoneScreen');
+const codeScreen = document.getElementById('codeScreen');
+const container = document.querySelector('.container');
+const chatPage = document.querySelector('.chat-page');
+
+phoneScreenButton.addEventListener('click', goToCodeScreen);
+confirmCodeButton.addEventListener('click', confirmCode);
+
+// Скрываем все экраны, кроме экрана ввода номера телефона, при загрузке страницы
+codeScreen.style.display = 'none';
+container.style.display = 'none';
+chatPage.style.display = 'none';
